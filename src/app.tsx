d0c11f867bb3a2d5147ec98c0cc7a0b4335d3c16
Yago Sousa <yagoayato666@gmail.com>
2024-02-09 10:50:00 -0300

    adiciona transcrição por áudio

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent, useState } from "react"
 import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
-import { NoteCard } from "./components/note-card"
+import { NoteCard } from "./components/note-card"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
 
 interface Note {
   id: string
@@ -36,18 +37,28 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
       const query = event.target.value
 
       setSearch(query)
   }
 
-  const filteredNotes = search != '' 
+  const filteredNotes = search !== '' 
     ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NWL Expert Logo"/>
 
       <form className="w-full">
@@ -61,11 +72,11 @@ export function App() {
 
       <div className="h-px bg-slate-700">
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px] pt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] pt-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
         })}
       </div>
       </div>  
